12441cb027982764a6bb39d6b344ee15117f12df
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2025-08-16 12:13:01 -0700

    Change Filament Primary Color

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -33,7 +33,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->colors([
-                'primary' => Color::hex("#62b344"),
+                'primary' => Color::hex("#61b346"),
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
