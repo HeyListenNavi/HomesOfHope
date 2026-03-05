09f1641a5068cf66385a45d5cb9538e4b5d1a86f
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2026-03-04 23:08:44 -0800

    update: send applicant reminder job to use last_reminded_at instead of using the last message to check the last reminder

--- app/Jobs/SendApplicantReminder.php
+++ app/Jobs/SendApplicantReminder.php
@@ -34,34 +34,38 @@ class SendApplicantReminder implements ShouldQueue
         if (!$applicant) return;
 
         $applicant->load('conversation.latestMessage');
-
         $conversation = $applicant->conversation;
 
         if (!$conversation || !$conversation->latestMessage) return;
 
         $latestUserMessage = $conversation->messages()->where('role', 'user')->latest('created_at')->first();
-
         $referenceTime = $latestUserMessage ? $latestUserMessage->created_at : $applicant->created_at;
 
-        $hoursSinceLastMessage = $referenceTime->diffInHours(now());
+        $hoursSinceLastReminder = $applicant->last_reminded_at ? $applicant->last_reminded_at->diffInHours(now()) : 0;
 
         if ($applicant->reminder_level === 0 && $hoursSinceLastMessage >= 22) {
             $notificationService->sendCustomMessage($applicant, 'Hola! Somos parte del equipo de Casas de Esperanza, seguimos esperando tu respuesta para continuar con tu proceso. Si tienes alguna duda o necesitas ayuda, no dudes en escribirnos. ¡Estamos aquí para apoyarte! ❤️', 'primer_recontacto');
 
-            $applicant->update(['reminder_level' => 1]);
+            $applicant->update([
+                'reminder_level' => 1,
+                'last_reminded_at' => now()
+            ]);
             return;
         }
 
-        if ($applicant->reminder_level === 1 && $hoursSinceLastMessage >= 48) {
+        if ($applicant->reminder_level === 1 && $hoursSinceLastReminder >= 48) {
             $notificationService->sendCustomMessage($applicant, 'Hola! Somos parte del equipo de Casas de Esperanza, te mandamos este mensaje para recordarte que tienes pendiente terminar con tu aplicación, tienes 3 días para continuar tu proceso de aplicación antes de que sea cancelada, esperamos tu respuesta ❤️', 'recontacto_final');
 
-            $applicant->update(['reminder_level' => 2]);
+            $applicant->update([
+                'reminder_level' => 2,
+                'last_reminded_at' => now()
+            ]);
             return;
         }
 
-        if ($applicant->reminder_level === 2 && $hoursSinceLastMessage >= 72) {
+        if ($applicant->reminder_level === 2 && $hoursSinceLastReminder >= 72) {
             $applicant->update([
-                'process_status' => 'canceled',
+                'process_status' => 'canceled'
             ]);
             return;
         }
