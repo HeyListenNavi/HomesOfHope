3a33a7f7a93bb4b14c45300ad4b001d06c373472
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2026-03-18 17:57:34 -0700

    style: move all applicants tab to the beginning

--- app/Filament/Resources/ApplicantResource/Pages/ListApplicants.php
+++ app/Filament/Resources/ApplicantResource/Pages/ListApplicants.php
@@ -22,20 +22,24 @@ class ListApplicants extends ListRecords
     public function getTabs(): array
     {
         return [
+            'todos' => Tab::make('Todos')
+                ->icon('heroicon-o-list-bullet')
+                ->modifyQueryUsing(fn (Builder $query) => $query),
+
             'aprobados' => Tab::make('Aprobados')
                 ->icon('heroicon-o-cpu-chip')
                 ->modifyQueryUsing(fn (Builder $query) => $query->where('process_status', 'approved')),
-            
+
             'aprobados_staff' => Tab::make('Aprobados por Staff')
                 ->icon('heroicon-o-check-circle')
                 ->modifyQueryUsing(fn (Builder $query) => $query->where('process_status', 'staff_approved')),
 
             'rechazados' => Tab::make('Rechazados')
-                ->icon('heroicon-o-cpu-chip') 
+                ->icon('heroicon-o-cpu-chip')
                 ->modifyQueryUsing(fn (Builder $query) => $query->where('process_status', 'rejected')),
 
             'rechazados_staff' => Tab::make('Rechazados por Staff')
-                ->icon('heroicon-o-x-circle') 
+                ->icon('heroicon-o-x-circle')
                 ->modifyQueryUsing(fn (Builder $query) => $query->where('process_status', 'staff_rejected')),
 
             'en_proceso' => Tab::make('En Progreso')
@@ -45,14 +49,10 @@ class ListApplicants extends ListRecords
             'requiere_revision' => Tab::make('Requiere Revision')
                 ->icon('heroicon-o-exclamation-triangle')
                 ->modifyQueryUsing(fn (Builder $query) => $query->where('process_status', 'requires_revision')),
-            
+
             'canceled' => Tab::make('Cancelado')
                 ->icon('lucide-ban')
                 ->modifyQueryUsing(fn (Builder $query) => $query->where('process_status', 'canceled')),
-
-            'todos' => Tab::make('Todos')
-                ->icon('heroicon-o-list-bullet')
-                ->modifyQueryUsing(fn (Builder $query) => $query),
         ];
     }
 }
