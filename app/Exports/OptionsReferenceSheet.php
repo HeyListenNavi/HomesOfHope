f01e9bc99e873c06aa3035b4e7e7e1ed6d61279d
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2026-05-16 18:28:00 -0700

    fix(sheets export): update allowed values example sheet

--- app/Exports/OptionsReferenceSheet.php
+++ app/Exports/OptionsReferenceSheet.php
@@ -21,15 +21,15 @@ class OptionsReferenceSheet implements FromCollection, ShouldAutoSize, WithHeadi
     {
         return new Collection([
             ['Campo', 'Valores Permitidos (Usar el código)', 'Descripción'],
-            ['status', 'new, approved, in_process, not_eligible, potential, built, dont_build', 'Nuevo, Aprobado, En Espera, No Califica, Potencial, Construido, No Construir'],
-            ['land_currency', 'mxn, usd', 'Pesos Mexicanos, Dólares'],
-            ['home_status', 'rented, borrowed, other', 'Rentada, Prestada, Otro'],
-            ['home_roof_condition', 'good, fair, poor', 'Bueno, Regular, Malo'],
-            ['home_floor_condition', 'good, fair, poor', 'Bueno, Regular, Malo'],
-            ['home_walls_condition', 'good, fair, poor', 'Bueno, Regular, Malo'],
-            ['home_bathroom_location', 'inside, outside', 'Adentro, Afuera'],
-            ['Booleans (si/no)', '1, true, yes, si', 'Cualquiera de estos se toma como VERDADERO. Vacío o 0 es FALSO.'],
-            ['Servicios', 'electricity, water, septic_tank, sewage', 'Luz, Agua, Fosa Séptica, Drenaje. Separar con comas si hay más de uno.'],
+            ['Estatus', 'new, approved, in_process, not_eligible, potential, built, dont_build', 'Nuevo, Aprobado, En Espera, No Califica, Potencial, Construido, No Construir'],
+            ['Moneda de pagos del Terreno', 'mxn, usd', 'Pesos Mexicanos, Dólares'],
+            ['Estado de la Casa', 'rented, borrowed, other', 'Rentada, Prestada, Otro'],
+            ['Estado del Techo de la Casa', 'good, fair, poor', 'Bueno, Regular, Malo'],
+            ['Estado del Piso de la Casa', 'good, fair, poor', 'Bueno, Regular, Malo'],
+            ['Estado de las Paredes de la Casa', 'good, fair, poor', 'Bueno, Regular, Malo'],
+            ['Ubicación del Baño de la Casa', 'inside, outside', 'Adentro, Afuera'],
+            ['Valores de si/no', '1, true, yes, si', 'Cualquiera de estos se toma como VERDADERO. Vacío o 0 es FALSO.'],
+            ['Servicios del Terreno', 'electricity, water, septic_tank, sewage', 'Luz, Agua, Fosa Séptica, Drenaje. Separar con comas si hay más de uno.'],
             ['Fechas', 'DD/MM/YYYY', 'Ejemplo: 12/05/2026'],
         ]);
     }
