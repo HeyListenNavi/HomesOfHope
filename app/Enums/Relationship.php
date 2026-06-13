36a512c6aa22ccc00e5ad0c178bdac4665878eb3
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2026-06-12 17:04:38 -0700

    add: tutor to relationship enum

--- app/Enums/Relationship.php
+++ app/Enums/Relationship.php
@@ -5,13 +5,14 @@ namespace App\Enums;
 use Filament\Support\Contracts\HasColor;
 use Filament\Support\Contracts\HasLabel;
 
-enum Relationship: string implements HasLabel, HasColor
+enum Relationship: string implements HasColor, HasLabel
 {
     case Father = 'padre';
     case Mother = 'madre';
     case Child = 'hijo';
     case Grandparent = 'abuelo';
     case Grandchild = 'nieto';
+    case Tutor = 'tutor';
     case Other = 'otro';
 
     public function getLabel(): ?string
@@ -22,6 +23,7 @@ enum Relationship: string implements HasLabel, HasColor
             self::Child => '👶 Hijo(a)',
             self::Grandparent => '👴 Abuelo(a)',
             self::Grandchild => '🧸 Nieto(a)',
+            self::Tutor => '🧑‍🏫 Tutor(a)',
             self::Other => '👤 Otro',
         };
     }
@@ -31,7 +33,7 @@ enum Relationship: string implements HasLabel, HasColor
         return match ($this) {
             self::Father, self::Mother => 'primary',
             self::Child, self::Grandchild => 'info',
-            self::Grandparent => 'warning',
+            self::Grandparent, self::Tutor => 'warning',
             self::Other => 'gray',
         };
     }
