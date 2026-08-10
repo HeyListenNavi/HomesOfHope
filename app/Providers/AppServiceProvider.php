bd7d0461b4cf0a646543cb137cc4ea035acfb2a1
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2026-08-10 00:00:18 -0700

    fix(js): filament map picker was not loading correctly because of double loading of leaflet and leaflet-geoman

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -70,10 +70,6 @@ class AppServiceProvider extends ServiceProvider
         Gate::policy(Role::class, RolePolicy::class);
 
         FilamentAsset::register([
-            Css::make('leaflet', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css'),
-            Js::make('leaflet', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'),
-            Css::make('leaflet-geoman', 'https://unpkg.com/@geoman-io/leaflet-geoman-free@latest/dist/leaflet-geoman.css'),
-            Js::make('leaflet-geoman', 'https://unpkg.com/@geoman-io/leaflet-geoman-free@latest/dist/leaflet-geoman.min.js'),
             Css::make('boxicons', 'https://cdn.boxicons.com/3.0.8/fonts/basic/boxicons.min.css'),
             Css::make('boxicons-filled', 'https://cdn.boxicons.com/3.0.8/fonts/filled/boxicons-filled.min.css'),
             Css::make('boxicons-brands', 'https://cdn.boxicons.com/3.0.8/fonts/brands/boxicons-brands.min.css'),
